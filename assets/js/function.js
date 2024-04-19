f3a61e2e554d1f126a810c99d4a199a4915dce07
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-04-19 20:23:20 -0300

    update readme

---
+++ assets/js/function.js
@@ -0,0 +1,32 @@
+function validateForm() {
+    // Obter os valores dos campos de e-mail e senha
+    var email = document.getElementById("email-tel").value;
+    var password = document.getElementById("pass").value;
+
+    // Verificar se os campos estão preenchidos
+    if (email.trim() === "" || password.trim() === "") {
+        alert("Por favor, preencha todos os campos.");
+        return false; // Impede o envio do formulário se os campos estiverem vazios
+    }
+
+    // Redireciona para a página home.html
+    window.location.href = "home.html";
+    return false;
+}
+function togglePassword(id) {
+    var x = document.getElementById(id);
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+document.getElementById("continuarButton").addEventListener("click", function() {
+    // Redireciona para a página home.html quando o botão for clicado
+    window.location.href = "home.html";
+});
+
+function mostrarPopup() {
+    alert("Registro salvo com sucesso! :)");
+}
