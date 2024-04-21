a057538b420900c0d5a588924c16b7bfa1baaf26
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-04-20 21:45:15 -0300

    esqueleto do projeto

--- js/script.js
+++ js/script.js
@@ -27,3 +27,58 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
+// permission
+var $status = document.getElementById('status');
+
+if ('Notification' in window) {
+  $status.innerText = Notification.permission;
+}
+
+function requestPermission() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  Notification.requestPermission(function (result) {
+    $status.innerText = result;
+  });
+}
+
+function nonPersistentNotification() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  try {
+    var notification = new Notification("Hi there - non-persistent!");
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
+}
+
+function persistentNotification() {
+  if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
+    alert('Persistent Notification API not supported!');
+    return;
+  }
+  
+  try {
+    navigator.serviceWorker.getRegistration()
+      .then((reg) => reg.showNotification("Hi there - persistent!"))
+      .catch((err) => alert('Service Worker registration error: ' + err));
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
+// exemplo
+// Exemplo de uso
+spawnNotification({
+    title: "Bem vindo ao MindCalm!",
+    opt: {
+        body: "Espero que você esteja desfrutando de nossos serviços!",
+        icon: "icons/playstore.png", // Ícone opcional
+    },
+    link: "/detalhes-da-venda" // Link opcional
+}
+)(jQuery);}
